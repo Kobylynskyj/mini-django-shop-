9c187f101d0865fd552960855d37c1275077df2e
dima <kobylynskyjd@gmail.com>
2026-07-23 11:40:44 +0300

    feat: add admin configuration for all models

--- shop/admin.py
+++ shop/admin.py
@@ -3,11 +3,29 @@ from .models import Category, Product, Cart, CartItem, Order, OrderItem, Purchas
 
 # Register your models here.
 
-admin.site.register(Category)
-admin.site.register(Product)
+class CustomProduct(admin.ModelAdmin):
+    list_display = ["name", "price", "category", "created_date"]
+    list_filter = ["category"]
+    search_fields = ["name", "description"]
+
+
+class CustomCategory(admin.ModelAdmin):
+    list_display = ["name", "created_date"]
+    search_fields = ["name"]
+
+
+class CustomOrder(admin.ModelAdmin):
+    list_display = ["id", "user", "total_price", "created_date"]
+    list_filter = ["user"]
+    search_fields = ["user__username"]
+
+
+
+admin.site.register(Category, CustomCategory)
+admin.site.register(Product, CustomProduct)
 admin.site.register(Cart)
 admin.site.register(CartItem)
-admin.site.register(Order)
+admin.site.register(Order, CustomOrder)
 admin.site.register(OrderItem)
 admin.site.register(Purchase)
 
